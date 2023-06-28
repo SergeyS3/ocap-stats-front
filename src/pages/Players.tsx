0a5c6eaafa19f4687ff35eac85f60f8377360885
Sergey Semichastnov <Sergey--S@yandex.ru>
2023-06-28 23:24:00 +0300

    refactor to react-query

--- src/pages/Players.tsx
+++ src/pages/Players.tsx
@@ -1,34 +1,25 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import useTitle from '../hooks/useTitle'
-import { BotApi } from '../services/bot-api'
+import { fetchPlayers } from '../services/bot-api'
 import Block from '../layouts/Block'
 import Table from '../layouts/Table/Table'
 import { playersTableCols } from '../data/tables/players'
 import { ProjectContext } from '../App'
 import Loader from '../components/Loader'
+import { useQuery } from 'react-query'
 
 
 const Players = () => {
   useTitle('Игроки')
 
   const project = useContext(ProjectContext)
-  const [playersStat, setPlayersStat] = useState([] as Player[])
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    setIsLoading(true)
-
-    void BotApi.players(project).then(players => {
-      setIsLoading(false)
-      setPlayersStat(players)
-    })
-  }, [project])
+  const { isLoading, error, data: players } = useQuery<Player[]>(['players', project], () => fetchPlayers(project))
 
   return (
     <Block>
-      {isLoading
+      {isLoading || error
         ? <Loader />
-        : <Table cols={playersTableCols} rows={playersStat} defaultSortField='frags' />
+        : <Table cols={playersTableCols} rows={players!} defaultSortField='frags' />
       }
     </Block>
   )
