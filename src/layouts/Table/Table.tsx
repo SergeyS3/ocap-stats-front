6f32d6ea4d212bf9f13754510b0563839b24e4a4
Sergey Semichastnov <Sergey--S@yandex.ru>
2023-11-12 20:42:04 +0300

    made table smaller, added reload btn

--- src/layouts/Table/Table.tsx
+++ src/layouts/Table/Table.tsx
@@ -3,35 +3,54 @@ import './Table.css'
 import Pagination from '../../components/Pagination'
 import TableFilter from './TableFilter'
 import TableHead from './TableHead'
+import type { UseQueryResult } from '@tanstack/react-query'
+import Loader from '../../components/Loader'
 
 
 export type Props<T> = {
   cols: AnyTableCols<T>
   rows: T[]
+  isFetching: boolean
+  refetch: UseQueryResult['refetch']
   defaultSortField: NonNullable<BaseTableCol<T>['sortField']>
 }
 
-const Table = <T, >({ cols, rows, defaultSortField }: Props<T>) => {
+const Table = <T, >({ cols, rows, isFetching, refetch, defaultSortField }: Props<T>) => {
   const [pageRows, setPageRows] = useState([] as T[])
   const [filteredRows, setFilteredRows] = useState([] as T[])
   const [sortedRows, setSortedRows] = useState([] as T[])
 
   return (
     <>
-      <TableFilter cols={cols} rows={rows} onChange={setFilteredRows} />
+      <div className='table-toolbar'>
+        <TableFilter cols={cols} rows={rows} onChange={setFilteredRows} />
+        <div>
+          <button
+            className='form-control'
+            type='button'
+            style={{ fontSize: 20 }}
+            onClick={() => void refetch()}
+          >
+            ↻
+          </button>
+        </div>
+      </div>
       <table>
         <TableHead cols={cols} rows={filteredRows} defaultSortField={defaultSortField} onSortChange={setSortedRows} />
-        <tbody>
-          {pageRows.map((row, i) =>
-            <tr key={i}>
-              {cols.map(col =>
-                <td key={i + col.label}>{col.getVal(row)}</td>,
-              )}
-            </tr>,
-          )}
-        </tbody>
+        {!isFetching &&
+          <tbody>
+            {pageRows.map((row, i) =>
+              <tr key={i}>
+                {cols.map(col =>
+                  <td key={i + col.label}>{col.getVal(row)}</td>,
+                )}
+              </tr>,
+            )}
+          </tbody>
+        }
       </table>
-      <Pagination items={sortedRows} pageSize={20} onPageSet={setPageRows} />
+      {isFetching && <Loader />}
+      <Pagination items={sortedRows} pageSize={15} onPageSet={setPageRows} />
     </>
   )
 }
